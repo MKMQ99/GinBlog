41e52f09a466056ce64125cfb823290cbbdf670b
MKMQ99 <904423848@qq.com>
2022-11-30 17:36:38 +0800

    update database AddUser; dao GetUsers, CheckUpUser

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,6 +1,15 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"GinBlog/dao"
+	"GinBlog/model"
+	"GinBlog/utils/errmsg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+var code int
 
 // 查询用户是否存在
 func UerExist(c *gin.Context) {
@@ -9,17 +18,46 @@ func UerExist(c *gin.Context) {
 
 // 添加用户
 func AddUser(c *gin.Context) {
+	var data model.User
+	_ = c.ShouldBind(&data)
+	code = dao.CheckUser(data.Username)
+	if code == errmsg.SUCCSE {
+		dao.CreateUser(&data)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 
 }
 
 // 查询用户列表
 func GetUsers(c *gin.Context) {
-
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
+	if pageSize == 0 {
+		pageSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = 1
+	}
+	data := dao.GetUsers(pageSize, pageNum)
+	code = errmsg.SUCCSE
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // 编辑用户
 func EditUser(c *gin.Context) {
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBind(&data)
 
+	code := dao.CheckUpUser(id, data.Username)
 }
 
 // 删除用户
--- dao/user.go
+++ dao/user.go
@@ -3,6 +3,8 @@ package dao
 import (
 	"GinBlog/model"
 	"GinBlog/utils/errmsg"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // 查询用户是否存在
@@ -15,6 +17,19 @@ func CheckUser(username string) int {
 	return errmsg.SUCCSE
 }
 
+// CheckUpUser 更新查询
+func CheckUpUser(id int, name string) (code int) {
+	var user model.User
+	db.Select("id, username").Where("username = ?", name).First(&user)
+	if user.ID == uint(id) {
+		return errmsg.SUCCSE
+	}
+	if user.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED //1001
+	}
+	return errmsg.SUCCSE
+}
+
 // 新增用户
 func CreateUser(data *model.User) int {
 	err := db.Create(data).Error
@@ -22,4 +37,19 @@ func CreateUser(data *model.User) int {
 		return errmsg.ERROR
 	}
 	return errmsg.SUCCSE
+}
+
+// 查询用户列表
+func GetUsers(pageSize int, pageNum int) []model.User {
+	var users []model.User
+	err := db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return users
+}
+
+// 编辑用户
+func EditUser(c *gin.Context) {
+	
 }

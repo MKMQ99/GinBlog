6c8181aec911ff975504c5f910b309c95edb1434
MKMQ99 <904423848@qq.com>
2022-12-01 22:06:53 +0800

    original setting.go reveal secret, so update setting.go and clean history

---
+++ utils/setting.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+	JwtKey   string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassword string
+	DbName     string
+
+	Zone       string
+	AccessKey  string
+	SecretKey  string
+	Bucket     string
+	QiniuSever string
+)
+
+func init() {
+	file, err := ini.Load("../config/config.ini")
+	if err != nil {
+		fmt.Println("配置文件有误：", err)
+	}
+	LoadServer(file)
+	LoadData(file)
+	LoadQiniu(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("server").Key("AppMode").String()
+	HttpPort = file.Section("server").Key("HttpPort").String()
+	JwtKey = file.Section("server").Key("JwtKey").String()
+}
+
+func LoadData(file *ini.File) {
+	Db = file.Section("database").Key("Db").String()
+	DbHost = file.Section("database").Key("DbHost").String()
+	DbPort = file.Section("database").Key("DbPort").String()
+	DbUser = file.Section("database").Key("DbUser").String()
+	DbPassword = file.Section("database").Key("DbPassword").String()
+	DbName = file.Section("database").Key("DbName").String()
+}
+
+func LoadQiniu(file *ini.File) {
+	AccessKey = file.Section("qiniu").Key("AccessKey").String()
+	SecretKey = file.Section("qiniu").Key("SecretKey").String()
+	Bucket = file.Section("qiniu").Key("Bucket").String()
+	QiniuSever = file.Section("qiniu").Key("QiniuServer").String()
+}
